b27f25ba04f703744fb98515d4506a4c1610e412
thanhlam104 <91646846+thanhlamdang@users.noreply.github.com>
2022-07-21 22:07:56 +0700

    add requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit as st 
 
+
+if __name__=='__main__':
+    st.title("Restore Vietnamese accent marks")
+    instruction = 'Please enter the unaccented sentence'
+    st.write(instruction)
+
+    
