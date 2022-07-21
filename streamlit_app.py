3b8a86583f3d9a0225fed905c865ea6e52c52955
Dang Thanh Lam <69765444+thanhlam104@users.noreply.github.com>
2022-07-21 23:03:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 if __name__=='__main__':
     st.title("Restore Vietnamese accent marks")
     instruction = 'Please enter the unaccented sentence'
-    st.write(instruction)
+    #st.write(instruction)
     
     input = st.text_input("Your text:")
 
